219b3cb444b146aae6ca27651f128aa8756c1c39
SamayaA <asadullaevasamaya@gmail.com>
2022-05-08 19:43:46 +0300

    Page to register employees actions

--- workers_table/views.py
+++ workers_table/views.py
@@ -8,16 +8,19 @@ from diploma.settings import TEMPLATE_DIR
 def workers(request):
     content = {
         "n": range(10),
+        "status": 'done',
     }
     # soup = BeautifulSoup(open(TEMPLATE_DIR + '\index.html'), 'html.parser')
-    # print(soup.prettify())
     # h1 = soup.find_all('h1')
-    # print(h1)
-    # for i in h1:
-    #     classes = i.get('class','')
-    #     if 'h1' not in classes:
-    #         i['class'] = i.get('class','') + ' h1'
-    # print(soup)
-    # with open(TEMPLATE_DIR + '\index1.html','w') as file:
-    #     file.writelines(soup)
     return render(request,"index.html", content)
+
+# def change_color():
+#     soup = BeautifulSoup(open(TEMPLATE_DIR + '\index1.html'), 'html5lib')
+#     h1 = soup.find_all(tag)
+
+def homepage(request):
+    content = {
+        "user": "User",
+        "done_hours": "40",
+    }
+    return render(request,"homepage.html", content)
