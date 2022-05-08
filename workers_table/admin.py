72ae68fe30944cbf71d2ef2dce5729279d87fbfb
SamayaA <asadullaevasamaya@gmail.com>
2022-05-08 19:48:00 +0300

    Added models to admin panel

--- workers_table/admin.py
+++ workers_table/admin.py
@@ -1,3 +1,25 @@
+
 from django.contrib import admin
 
+from workers_table.models import Department, DepartmentHead, Employee, Position, Control
+
 # Register your models here.
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = [all]
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = [all]
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = [all]
+
+@admin.register(DepartmentHead)
+class DepartmentHeadAdmin(admin.ModelAdmin):
+    list_display = [all]
+
+@admin.register(Control)
+class ControlAdmin(admin.ModelAdmin):
+    list_display = [all]
