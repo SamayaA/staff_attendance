156c15156cf3a0c40386dbdb6e15baa7b1090c28
SamayaA <asadullaevasamaya@gmail.com>
2022-05-15 22:52:42 +0300

    Changed related names and deleted ABSENT choice in Control model

--- workers_table/models.py
+++ workers_table/models.py
@@ -12,12 +12,18 @@ class Department(models.Model):
     name = models.CharField(verbose_name="department_name", blank=False, max_length=50)
     phone = PhoneNumberField()
 
+    def __str__(self):
+        return self.name
+
 class Position(models.Model):
     '''
     Position
     args: name: CharField() 
     '''
-    name = models.CharField(verbose_name="position", blank=False, max_length=50)
+    name = models.CharField(verbose_name="position_name", blank=False, max_length=50)
+    
+    def __str__(self):
+        return self.name
 
 class Employee(User):
     '''
@@ -27,8 +33,11 @@ class Employee(User):
     department: Department ID, position: Position ID,
     '''
     phone = PhoneNumberField()
-    department = models.ForeignKey(Department, related_name="department", on_delete=models.DO_NOTHING)
+    department = models.ForeignKey(Department, related_name="employee", on_delete=models.DO_NOTHING)
     position = models.ForeignKey(Position, related_name="position",on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}' 
 
 class DepartmentHead(models.Model):
     '''
@@ -36,15 +45,14 @@ class DepartmentHead(models.Model):
     args: head: Employee ID, department: Department ID 
     '''
     # Employee
-    head = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name="head", on_delete=models.CASCADE, blank=False)
-    department = models.ForeignKey(Department, verbose_name="department", on_delete=models.CASCADE, blank=False)
+    head = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="department_head", verbose_name="head", on_delete=models.CASCADE, blank=False)
+    department = models.ForeignKey(Department, related_name="department_head", on_delete=models.CASCADE, blank=False)
 
 class EmployeeStatusChoices(models.TextChoices):
     '''Status of employee movement'''
 
     ARRIVED = "ARRIVED", "Прибыл"
     LEFT = "LEFT", "Убыл"
-    ABSENT = "ABSENT", "Отсутствует"
 
 
 class Control(models.Model):
@@ -54,12 +62,12 @@ class Control(models.Model):
     time: register time
     status: status of employee movement
     '''
-    employee = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, blank=False)
+    employee = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="control", on_delete=models.DO_NOTHING, blank=False)
     date = models.DateField(verbose_name="date", auto_now_add=True, blank=False)
     time = models.TimeField(verbose_name="time", auto_now_add=True,blank=False)
     status = models.TextField(
         choices=EmployeeStatusChoices.choices,
-        default=EmployeeStatusChoices.ABSENT
+        default=EmployeeStatusChoices.ARRIVED
         )
     reason = models.TextField(default='')
 
@@ -68,7 +76,7 @@ class Exceptions(models.Model):
     News about time changing in schedule
     date: that date is exception
     time: how long would it be
-    name: reason for exeption
+    name: reason for exception
     '''
     date = models.DateField(verbose_name="date", auto_now_add=True, blank=False, unique=True)
     time = models.TimeField(verbose_name="time", auto_now_add=True,blank=False)
@@ -82,7 +90,7 @@ class Vacation(models.Model):
     end_date: last day_off
     '''
     # Employee
-    employee = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.CASCADE)
+    employee = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="vacation", on_delete= models.CASCADE)
     start_date = models.DateField(verbose_name="start_date", auto_now_add=True, blank=False)
     end_date = models.DateField(verbose_name="end_date", auto_now_add=True, blank=False)
 
