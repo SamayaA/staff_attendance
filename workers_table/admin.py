6bb643f501521cf35b22b386aef6a3db8b8a48c8
SamayaA <asadullaevasamaya@gmail.com>
2022-05-15 22:42:07 +0300

    Defined all fields in admin panel

--- workers_table/admin.py
+++ workers_table/admin.py
@@ -1,25 +1,33 @@
 
 from django.contrib import admin
 
-from workers_table.models import Department, DepartmentHead, Employee, Position, Control
+from workers_table.models import Department, DepartmentHead, Employee, Position, Control, Vacation, Exceptions
 
 # Register your models here.
 @admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
-    list_display = [all]
+    list_display = ("id", "username", "first_name", "last_name", "phone", "department", "position")
 
 @admin.register(Department)
 class DepartmentAdmin(admin.ModelAdmin):
-    list_display = [all]
+    list_display = ("id", "name", "phone")
 
 @admin.register(Position)
 class PositionAdmin(admin.ModelAdmin):
-    list_display = [all]
+    list_display = ("id", "name")
 
 @admin.register(DepartmentHead)
 class DepartmentHeadAdmin(admin.ModelAdmin):
-    list_display = [all]
+    list_display = ("id", "head", "department")
 
 @admin.register(Control)
 class ControlAdmin(admin.ModelAdmin):
-    list_display = [all]
+    list_display = ("id", "employee", "date", "time", "status", "reason")
+
+@admin.register(Vacation)
+class VacationAdmin(admin.ModelAdmin):
+    list_display = ("id", "employee", "start_date", "end_date")
+
+@admin.register(Exceptions)
+class ExceptionsAdmin(admin.ModelAdmin):
+    list_display = ("id", "date", "name")    
