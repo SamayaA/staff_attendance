74fa1efd3fb27011fa1d9a2d3b7cdaba3d27de36
SamayaA <asadullaevasamaya@gmail.com>
2022-05-16 17:19:02 +0300

    Added placeholder and class to input forms of login

--- workers_table/forms.py
+++ workers_table/forms.py
@@ -1,5 +1,6 @@
+from turtle import right
 from django import forms
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label="Логин")
-    password = forms.CharField(label="Пароль", widget=forms.PasswordInput)
+    username = forms.CharField(label='', label_suffix='', required=True, widget=forms.TextInput(attrs={'placeholder':'Логин', 'class': 'from-input'}))
+    password = forms.CharField(label='', label_suffix='', required=True, widget=forms.PasswordInput(attrs={'placeholder':'Пароль', 'class': 'from-input'}))
