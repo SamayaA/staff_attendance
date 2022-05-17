b309935265d316b28fb62c6e07e9649756ed7cf4
SamayaA <asadullaevasamaya@gmail.com>
2022-05-18 00:23:01 +0300

    added verification of left status

--- workers_table/views.py
+++ workers_table/views.py
@@ -1,5 +1,5 @@
 
-# Create your views here.
+# подключение модулей
 import datetime
 from django.http import HttpResponse
 
@@ -19,6 +19,7 @@ from workers_table.serializers import ControlSerializer, EmployeeSerializer
 
 import requests
 
+# аутентификация пользователя
 def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
@@ -36,11 +37,13 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'workers_table/login.html', {'form': form})
-    
+
+# выход из аккаунта
 def logout(request):
     auth_logout(request)
     return redirect('login')
 
+# функция, которая выводит данные на странице "Таблица сотрудников" 
 def workers(request):
     '''
     returns the table of employees and their current status of being at work
@@ -58,7 +61,7 @@ def workers(request):
         return render(request,"workers_table/index.html", content)
     return redirect('login')
     
-
+# страница регистрации сотрудника
 def homepage(request, user=None):
     if not request.user.is_authenticated:
         return redirect('login')
@@ -67,29 +70,28 @@ def homepage(request, user=None):
     }
     return render(request,"workers_table/homepage.html", content)
 
+# функция, котрая выводит подробную информацию о сотруднике
 def employee_page(request):
-    employee_id = request.GET.get('id', None)
-    user = request.user
-    print(user)
-    if user == None:
-        return HttpResponse('Вы не авторизованы')
-    elif employee_id == None:
-        return HttpResponse('У данного сотрудника нет id')
-    else:
-        token = Token.objects.get(user=request.user)
-        headers = {
-            'accept': 'application/json' ,
-            'authorization': f'Token {token}'
-        }
-        responce = requests.get(f'http://127.0.0.1:8000/api/employees/{employee_id}/', headers=headers).json()
-        print(responce)
-        context = {
-            "employee": responce,
-        }
-        return render(request, "workers_table/employee.html", context)
+    if request.user.is_authenticated:
+        employee_id = request.GET.get('id', None)
+        user = request.user
+        if employee_id == None:
+            return HttpResponse('У данного сотрудника нет id')
+        else:
+            token = Token.objects.get(user=request.user)
+            headers = {
+                'accept': 'application/json' ,
+                'authorization': f'Token {token}'
+            }
+            responce = requests.get(f'http://127.0.0.1:8000/api/employees/{employee_id}/', headers=headers).json()
+            print(responce)
+            context = {
+                "employee": responce,
+            }
+            return render(request, "workers_table/employee.html", context)
+    return redirect('logout')
     
-
-
+# API сотрудников компании
 class EmployeeViewSet(ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
@@ -99,36 +101,46 @@ class EmployeeViewSet(ModelViewSet):
         Token.objects.create(user=self)
         serializer.save()
 
+# API всех дней
 class ControlViewSet(ModelViewSet):
     queryset = Control.objects.all()
     serializer_class = ControlSerializer
     filter_backends = [filters.DjangoFilterBackend]
     permission_classes = [IsAdminUser, ControlPermission]
 
+# API присутствующих и убывших сотрудников и их время в нынешний день
 class ControlThatDayViewSet(ModelViewSet):
-    # employees who left
+    # сотрудники которые покинули в этот день рабочее место
     queryset_left = Control.objects\
         .filter(date=datetime.date.today(), status="LEFT")
-    # empolees who arrived today and haven't left
+    # сотрудники, которые находятся на рабочем месте
     queryset_arrived = Control.objects\
         .filter(date=datetime.date.today(), status="ARRIVED")\
         .exclude(employee__in=queryset_left.values_list("employee"))
     queryset = queryset_left.union(queryset_arrived)
-    # queryset_absent = Employee.objects.exclude(id__in=queryset.values_list("employee"))
     serializer_class = ControlSerializer
     filter_backends = [filters.DjangoFilterBackend]
     permission_classes = [IsAuthenticated]
 
+# функции, которая отправляет регистрацию прибытия/убытие сотрудника
 def post_control(request):
     if request.user.is_authenticated:
         token = Token.objects.get(user=request.user)
+
+        # проверка был ли у сотрудника этот статус сегодня
         status_repeat = 0
         status_repeat = Control.objects.filter(employee_id=request.POST["employee_id"],
             status=request.POST["status"], date=datetime.date.today()).count()
-        print(status_repeat)
-        if status_repeat != 0:
-            return HttpResponse('Вы не можете выбрать данный статус')  
-        employee = Control(employee_id=request.POST["employee_id"],
-            status=request.POST["status"],reason=request.POST["reason"]).save()
-        
-        return redirect("list")
+        impossible_status = 1
+
+        # проверка при отправке статуса ушел, был ли отправлен в этот день статут прибыл
+        if request.POST["status"] == "LEFT":
+            impossible_status = Control.objects.filter(employee_id=request.POST["employee_id"],
+            status="ARRIVED", date=datetime.date.today()).count()
+        if impossible_status != 0 and status_repeat == 0:
+            employee = Control(employee_id=request.POST["employee_id"],
+                status=request.POST["status"],reason=request.POST["reason"]).save()
+            return redirect("list")
+        else:  
+            return HttpResponse('Вы не можете выбрать данный статус')
+            
